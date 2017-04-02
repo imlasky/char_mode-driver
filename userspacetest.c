19a7b18db67f31d7f4abc0b0b2b5e6ef45ef9885
Sean Reedy <seanreedy239@gmail.com>
2017-04-02 01:18:25 -0400

    Made user test a continuous loop for better testing. Removed trailing white space from stringToSend

--- userspacetest.c
+++ userspacetest.c
@@ -17,26 +17,49 @@ int main(){
 		perror("Failed to open the device...");
 		return errno;
 	}
-	printf("Type in a short string to send to the kernel module:\n");
-	//scanf("%[^\n]*c", stringToSend);                // Read in a string (with spaces)
-	fgets(stringToSend ,sizeof stringToSend, stdin);
-	printf("Writing message to the device [%s].\n", stringToSend);
-	ret = write(fd, stringToSend, strlen(stringToSend)); // Send the string to the LKM
-	if (ret < 0){
-		perror("Failed to write the message to the device.");
-		return errno;
-	}
-	  				   
-	printf("Press ENTER to read back from the device...\n");
-	getchar();
-		 
-	printf("Reading from the device...\n");
-	ret = read(fd, receive, BUFFER_LENGTH);        // Read the response from the LKM
-	if (ret < 0){
-		perror("Failed to read the message from the device.");
-		return errno;
-	}
-	printf("The received message is: [%s]\n", receive);
+	
+	int selection = 0; 
+	int len; // length of input to kernel module 
+	do 
+	{
+	    printf("Enter a number to make a selection\n");  
+	    printf("1- Send/write string to kernel module\n");
+	    printf("2- Read string from kernel module\n"); 
+	    printf("0- Exit program\n"); 
+	    scanf("%d%*c", &selection); 
+	    if (selection == 1)
+	    {
+		printf("Type in a short string to send to the kernel module:\n");
+		//scanf("%[^\n]*c", stringToSend);                // Read in a string (with spaces)
+		fgets(stringToSend, sizeof stringToSend, stdin);
+		len = strlen(stringToSend);
+		if (len > 0 && stringToSend[len-1] == '\n')
+		    stringToSend[--len] = '\0'; // remove trailing new line character 
+		printf("Writing message to the device [%s].\n", stringToSend);
+		ret = write(fd, stringToSend, strlen(stringToSend)); // Send the string to the LKM
+		if (ret < 0){
+			perror("Failed to write the message to the device.");
+			return errno;
+		}
+	    }
+	    else if(selection == 2)
+	    {
+		printf("Press ENTER to read back from the device...\n");
+		getchar();
+		printf("Reading from the device...\n");
+		ret = read(fd, receive, BUFFER_LENGTH);        // Read the response from the LKM
+		if (ret < 0){
+			perror("Failed to read the message from the device.");
+			return errno;
+		}
+		printf("The received message is: [%s]\n", receive);
+	    }
+	    else
+	    {
+		selection = 0; 
+	    }
+	} while (selection > 0);
+
 	printf("End of the program\n");
 	return 0;
 }
